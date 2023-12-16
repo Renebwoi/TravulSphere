bddc8f3328db538518bffbbd3ff3455b8a80efb3
Renebwoi <119928501+Renebwoi@users.noreply.github.com>
2023-12-16 04:19:29 +0100

    added the navigation bar for mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+function navDisplay(num){
+    if (num==0){
+        document.querySelector('.mobile-nav').style.display = "grid";
+    }
+    else{
+        document.querySelector('.mobile-nav').style.display = "none";
+    }
+}
